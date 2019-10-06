cef8b88ded5b272df4dca1ea17702b8836bdc383
kamauvick <kamauvick@outlook.com>
2019-10-06 18:57:59 +0300

    Add a test for the Category instance.

--- pics/tests.py
+++ pics/tests.py
@@ -1,14 +1,16 @@
 from django.test import TestCase
 
+from .models import *
+
 
 # Create your tests here.
 # Location category
 class TestCategory(TestCase):
     def setUp(self):
-        pass
+        self.new_category = Category(name='cars')
 
     def test_category_instance(self):
-        pass
+        self.assertTrue(isinstance(self.new_category, Category))
 
     def test_str_category(self):
         pass
