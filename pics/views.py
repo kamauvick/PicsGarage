872750efdca4365a5fcbc81ab8a9a7e2e10cd745
kamauvick <kamauvick@outlook.com>
2019-10-07 11:22:40 +0300

    Add Views to render the filtered searches.

--- pics/views.py
+++ pics/views.py
@@ -28,3 +28,29 @@ def search_results(request):
     else:
         message = 'Ooppss, You did not search for anything.'
         return render(request, 'search_results.html', locals())
+
+
+def filter_by_category(request):
+    if 'image' in request.GET and request.GET['image']:
+        search_term = request.GET.get('image')
+        print(search_term)
+        searched_photos = Image.search_by_category(search_term)
+        message = f'{search_term}'
+        params = {
+            'searched_photos': searched_photos,
+            'message': message,
+        }
+
+        return render(request, 'category.html', params)
+
+
+def filter_by_locale(request):
+    if 'image' in request.GET and request.GET['image']:
+        search_term = request.GET.get('image')
+        print(search_term)
+        searched_photos = Image.filter_by_location(search_term)
+        message = f'{search_term}'
+        params = {
+            'searched_photos': searched_photos,
+            'message': message,
+        }
